3a0de3f11107ea817abca367055662dc9595f782
Ville Heikkinen <ville.heikkinen98@hotmail.fi>
2023-12-07 17:54:34 +0200

    Refactored destructuring the message on host side

--- host-client/src/main.rs
+++ host-client/src/main.rs
@@ -18,43 +18,30 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Iterate to poll the eventloop for connection progress
     for (_, notification) in connection.iter().enumerate() {
         // if you want to see *everything*, uncomment:
-        // println!("Notification = {:#?}", notification);
         if let Ok(rumqttc::Event::Incoming(Packet::Publish(publish_data))) = notification {
             if publish_data.topic == "sensor_data/temperature" {
-                let id: &Result<[u8; 36], std::array::TryFromSliceError> =
-                    &publish_data.payload[0..36].try_into();
-
-                let data: &Result<[u8; 4], std::array::TryFromSliceError> =
-                    &publish_data.payload[36..40].try_into();
-
-                // println!("{:?}", data);
-
-                // println!("{} : {:?}", core::str::from_utf8(id).unwrap(), temp_val);
-                if let (Ok(data), Ok(id)) = (data, id) {
-                    let temp_val = f32::from_le_bytes(*data);
-                    if let Ok(id) = core::str::from_utf8(id) {
-                        println!("{} : {:?} C", id, temp_val);
-                    }
-                }
+                let (id, temp_val) = deconstruct_message(&publish_data.payload).unwrap();
+                println!("{} : {:?} C", id, temp_val);
             }
             if publish_data.topic == "sensor_data/humidity" {
-                let id: &Result<[u8; 36], std::array::TryFromSliceError> =
-                    &publish_data.payload[0..36].try_into();
-
-                let data: &Result<[u8; 4], std::array::TryFromSliceError> =
-                    &publish_data.payload[36..40].try_into();
-
-                // println!("{:?}", data);
-
-                // println!("{} : {:?}", core::str::from_utf8(id).unwrap(), temp_val);
-                if let (Ok(data), Ok(id)) = (data, id) {
-                    let hum_value = f32::from_le_bytes(*data);
-                    if let Ok(id) = core::str::from_utf8(id) {
-                        println!("{} : {:?} %", id, hum_value);
-                    }
-                }
+                let (id, hum_val) = deconstruct_message(&publish_data.payload).unwrap();
+                println!("{} : {:?} %", id, hum_val);
             }
         }
     }
     Ok(())
+}
+
+fn deconstruct_message(msg: &[u8]) -> Option<(&str, f32)> {
+    let id = msg[0..36].try_into();
+
+    let data: Result<[u8; 4], std::array::TryFromSliceError> = msg[36..40].try_into();
+
+    if let (Ok(data), Ok(id)) = (data, id) {
+        let temp_val = f32::from_le_bytes(data);
+        if let Ok(id) = core::str::from_utf8(id) {
+            return Some((id, temp_val));
+        }
+    }
+    None
 }
